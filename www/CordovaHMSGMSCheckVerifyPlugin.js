db09af89259a686839fa57aaba32ecb27f6562e3
Daniel Ideriba <danielideriba86@gmail.com>
2021-08-23 17:20:31 -0400

    fix

--- www/CordovaHMSGMSCheckVerifyPlugin.js
+++ www/CordovaHMSGMSCheckVerifyPlugin.js
@@ -1,5 +1,7 @@
 var exec = require("cordova/exec");
 
+
+
 module.exports = {
   isHmsAvailable: function (success, error, arg0) {
     exec(success, error, "CordovaHMSGMSCheckVerifyPlugin", "isHmsAvailable", [arg0]);
